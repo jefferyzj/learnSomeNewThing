8fa3a06b8d900064185e12da11cd5513c83effbd
jefferyzj <zjxudanfeng@gmail.com>
2024-11-18 19:48:05 -0800

    delete useless proj1

--- proj1/feature1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Rack, Layer, Position, Product
-
-admin.site.register(Category)
-admin.site.register(Rack)
-admin.site.register(Layer)
-admin.site.register(Position)
-admin.site.register(Product)
--- proj1/feature1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Feature1Config(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "feature1"
